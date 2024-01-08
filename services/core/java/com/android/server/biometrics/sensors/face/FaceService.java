59e3c09f903898138ff3815f559455c179efe009
someone5678 <someone5678@users.noreply.github.com>
2024-01-08 18:42:13 +0900

    FaceService: Conditionally add ParanoidSense
    * We allows devices to use their own FaceService as ParanoidSense has weak modality
    * To make above impl complete, conditionally add ParanoidSense and un-comment AIDL provider support
    
    Change-Id: I7321580b87499f4020d3b080779e2f9cb1a6fce6

--- services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
+++ services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
@@ -659,6 +659,21 @@ public class FaceService extends SystemService {
                     new ClientMonitorCallbackConverter(receiver), opPackageName);
         }
 
+        private List<ServiceProvider> getSenseProviders() {
+            final List<ServiceProvider> providers = new ArrayList<>();
+            FaceSensorPropertiesInternal props = new FaceSensorPropertiesInternal(
+                    SenseProvider.DEVICE_ID,
+                    SensorProperties.STRENGTH_WEAK,
+                    1, /** maxEnrollmentsPerUser **/
+                    new ArrayList(),
+                    FaceSensorProperties.TYPE_RGB,
+                    false, /** supportsFaceDetection **/
+                    false, /** supportsSelfIllumination **/
+                    false); /** resetLockoutRequiresChallenge **/
+            SenseProvider provider = new SenseProvider(getContext(), mBiometricStateCallback, props, mLockoutResetDispatcher);
+            providers.add(provider);
+            return providers;
+        }
         @android.annotation.EnforcePermission(android.Manifest.permission.USE_BIOMETRIC_INTERNAL)
         public void registerAuthenticators(
                 FaceSensorConfigurations faceSensorConfigurations) {
@@ -674,6 +689,10 @@ public class FaceService extends SystemService {
         private List<ServiceProvider> getProviders(
                 FaceSensorConfigurations faceSensorConfigurations) {
             final List<ServiceProvider> providers = new ArrayList<>();
+            if (SenseUtils.canUseProvider()) {
+                providers.addAll(getSenseProviders());
+                return providers;
+            }
             final Pair<String, SensorProps[]> filteredSensorProps = filterAvailableHalInstances(
                             faceSensorConfigurations);
             providers.add(mFaceProviderFunction.getFaceProvider(filteredSensorProps,
